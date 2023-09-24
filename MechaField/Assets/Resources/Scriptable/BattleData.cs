c22675d91d53cb297cb30dbb0d9cf88dda8f1225
Hungry658 <Hungry658@users.noreply.github.com>
2023-09-24 18:56:03 +0900

    유니티 메카필드 삭제

--- MechaField/Assets/Resources/Scriptable/BattleData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "BattleData", menuName = "Scriptable Object/BattleData", order = 1)]
-public class BattleData : ScriptableObject
-{
-    public LayerMask layermask_actor;
-}
